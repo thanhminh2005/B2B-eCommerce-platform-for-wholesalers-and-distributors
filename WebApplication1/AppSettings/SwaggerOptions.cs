084c8acec215eb0b190009d965231599df6c2bc5
Nguyễn Thanh Minh <68472650+thanhminh2005@users.noreply.github.com>
2021-09-18 18:15:49 +0700

    fix gitigore

--- WebApplication1/AppSettings/SwaggerOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace B2B.AppSettings
-{
-    public class SwaggerOptions
-    {
-        public string JsonRoute { get; set; }
-        public string Description { get; set; }
-        public string UiEndpoint { get; set; }
-    }
-}
