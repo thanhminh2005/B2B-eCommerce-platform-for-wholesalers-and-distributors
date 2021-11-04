ba8be0f798fe5c87e82b6ea45f5f5a1994bd6cca
Nguyễn Thanh Minh <68472650+thanhminh2005@users.noreply.github.com>
2021-11-04 13:01:19 +0700

    fix ipn request prop type and complete momo payment

--- WebApplication1/MoMo/IPNRequest.cs
+++ WebApplication1/MoMo/IPNRequest.cs
@@ -6,9 +6,9 @@
         public string OrderId { get; set; }
         public string RequestId { get; set; }
         public long Amount { get; set; }
-        public long OrderInfo { get; set; }
+        public string OrderInfo { get; set; }
         public string OrderType { get; set; }
-        public int TransId { get; set; }
+        public long TransId { get; set; }
         public int ResultCode { get; set; }
         public string Message { get; set; }
         public string Paytype { get; set; }
