54975ec2785c98eae5611e1a3fc937eac06b6c19
Nguyễn Thanh Minh <68472650+thanhminh2005@users.noreply.github.com>
2021-11-10 09:39:44 +0700

    order request change

--- WebApplication1/DTOs/Orders/GetOrdersRequest.cs
+++ WebApplication1/DTOs/Orders/GetOrdersRequest.cs
@@ -3,6 +3,7 @@
     public class GetOrdersRequest
     {
         public string SessionId { get; set; }
+        public string DistributorId { get; set; }
         public int? Status { get; set; }
     }
 }
